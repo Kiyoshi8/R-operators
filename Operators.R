24c8f15760889c8b457a5eb1b1feee8904d6f23a
Kiyoshi C. Rio <86674319+Kiyoshi8@users.noreply.github.com>
2023-09-06 19:40:05 +0800

    Add files via upload

---
+++ Operators.R
@@ -0,0 +1,36 @@
+# OPERATORS IN R #
+
+
+# basic operators
+
+4+4
+4-4
+4*4
+4/4
+sqrt(4)
+4^4
+4**8
+
+
+10 %% 3 # modules division
+10 %/% 3 # integer division
+
+
+# logical operators
+
+a = 3:5
+b = 5:7
+
+a > 4
+a < 4
+a <= 4
+a >= 4
+
+a == 4
+a != 4
+
+
+# combine logical operations
+
+(a==4) & (b==6)
+(a==4) | (b>5)
